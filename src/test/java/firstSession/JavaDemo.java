7fb4965f03392f119781273a6f39b96b8f3f3634
SajidSaiyyad <sajidsaiyyad96@gmail.com>
2023-05-25 02:09:20 +0530

    added java code and changes in porm.xml

--- src/test/java/firstSession/JavaDemo.java
+++ src/test/java/firstSession/JavaDemo.java
@@ -2,8 +2,10 @@ package firstSession;
 
 public class JavaDemo {
 
-	public static void main(String[] args) {
-		System.out.println("I am writing first java code line-");
+	public static void main(String[] args)
+	{
+		System.out.println("I am writing first java code line-"
+				+ " lets write"	);
 
 	}
 
---
+++ src/test/java/firstSession/JavaDemo_1.java
@@ -0,0 +1,10 @@
+package firstSession;
+
+public class JavaDemo_1{
+
+	public static void main(String[] args) {
+		System.out.println("I am writing first java code line-");
+
+	}
+
+}
